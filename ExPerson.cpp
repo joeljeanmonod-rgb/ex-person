0f88491226d6006dc1b1564c1c56897674f309ba
joeljeanmonod-rgb <joel.jeanmonod@heig-vd.ch>
2023-03-13 12:02:14 +0100

    up

--- ExPerson.cpp
+++ ExPerson.cpp
@@ -1,16 +1,72 @@
 
 #include <iostream>
+
+
 class Person {
+public:
+    Person(std::string name,int age):
+    name_(name),age_(age){}
 
+	std::string getName()
+    {
+        return this->name_;
+    }
+
+
+protected:
+	std::string name_;
+    int age_;
 };
 
-class Student {
+class Student : public Person{
 
+public:
+    Student(std::string name, int age,double average) :
+	Person(name, age), average_(average){}
+
+    void printInfos() const
+    {
+        std::cout << "Name      : " << name_    << std::endl;
+        std::cout << "Age       : " << age_     << std::endl;
+        std::cout << "Average   : " << average_ << std::endl;
+    }
+
+
+private:
+    double average_;
 };
 
-class Professor {
+class Professor :public Person
+{
+public:
+    Professor(std::string name, int age, int EmployeeId) :
+    Person(name,age),EmployeeId_(EmployeeId) {}
 
+    void printInfos() const
+    {
+        std::cout << "Name          : " << name_        << std::endl;
+        std::cout << "Age           : " << age_         << std::endl;
+        std::cout << "Employee Id   : " << EmployeeId_   << std::endl;
+    }
+private:
+    int EmployeeId_;
 };
+
+
+class Room {
+public:
+
+
+
+
+
+
+
+private:
+    std::string name_;
+    Professor& prof_;
+};
+
 
 class School {
 
@@ -19,7 +75,7 @@ class School {
 int main()
 {
     std::cout << "Start school exercice\n";
-
+    
     School s;
     // s.addStudent() // add student 1
     // s.addStudent() // add student 2
